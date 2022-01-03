59ce24ad18da8f0e95e68a47e1256da44ac207b6
alejociro <alejandro.castrillonc@udea.edu.co>
2022-01-02 23:24:51 -0500

    fix: corrected errors
    correction of errors in the code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -42,22 +42,23 @@ class UserController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+
+
     public function store(Request $request)
     {
-        $this->validate($request, [
-            'name'=>'required',
-            'email'=> 'required|email|unique:users,email',
-            'password'=>'required|same:confirm-password',
-            'roles'=>'required'
+        $this->validate($request,[
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|same:confirm-password',
+            'roles' => 'required'
         ]);
-
         $input = $request->all();
         $input['password'] = Hash::make($input['password']);
 
-        $user = User::created($input);
-        $user->assingRole($request->input);
+        $user = User::create($input);
+        $user->assignRole($request->input('roles'));
 
-        return redirect()->rout('users.index');
+        return redirect()->route('users.index');
     }
 
     /**
@@ -97,7 +98,7 @@ class UserController extends Controller
     {
         $this->validate($request, [
             'name'=>'required',
-            'email'=> 'required|email|unique:users,email'.$id,
+            'email'=> 'required|email|unique:users,email,'.$id,
             'password'=>'same:confirm-password',
             'roles'=>'required'
         ]);
@@ -111,7 +112,7 @@ class UserController extends Controller
 
         $user = User::find($id);
         $user->update($input);
-        DB::table('model_has_roles')->where('model_id',$id)-delete();
+        DB::table('model_has_roles')->where('model_id',$id)->delete();
 
         $user->assignRole($request->input('roles'));
         return redirect()->route('users.index');
