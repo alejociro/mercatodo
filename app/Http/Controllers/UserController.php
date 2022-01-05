14cc5f2e9c11708f46c17ecb60cedb866f9b6233
alejociro <alejandro.castrillonc@udea.edu.co>
2022-01-05 00:07:32 -0500

    refactor: code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,7 +33,7 @@ class UserController extends Controller
     public function create()
     {
         $roles = Role::pluck('name','name')->all();
-        return view('users.create', compact('roles'));
+        return view('users.create', ['roles'=> $roles]);
     }
 
     /**
