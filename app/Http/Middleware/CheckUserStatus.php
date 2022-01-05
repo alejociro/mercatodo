646df697e0f1016748fd4b94cc37a3c0f2b807fb
alejociro <alejandro.castrillonc@udea.edu.co>
2022-01-05 00:05:21 -0500

    fix:. log out
    corrected the fix in log out for accounts dissableds

--- app/Http/Middleware/CheckUserStatus.php
+++ app/Http/Middleware/CheckUserStatus.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CheckUserStatus
 {
@@ -20,6 +21,12 @@ class CheckUserStatus
         {
             return $next($request);
         }else{
+            Auth::guard('web')->logout();
+
+            $request->session()->invalidate();
+
+            $request->session()->regenerateToken();
+
             return redirect('disabled');
         }
 
