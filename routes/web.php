d9d83cb4f5443a90899d5272d7096df44e02e759
alejociro <alejandro.castrillonc@udea.edu.co>
2022-02-05 12:06:57 -0500

    Feat: Add product routes
    Add the product resource route and change product status

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Controllers\ChangeUserStatusController;
+use App\Http\Controllers\ChangeProductStatusController;
+use App\Http\Controllers\ProductUserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RolController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -21,19 +24,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth','verified','check_Status'])->name('dashboard');
+
+//Route::controller(ProductUserController::class)->group(function () {
+//    Route::get('/productos', 'index');
+//});
 
 require __DIR__.'/auth.php';
 
 Route::group(['middleware'=>['auth','verified','check_Status']],function (){
     Route::resource('roles',RolController::class);
     Route::resource('users', UserController::class);
+    Route::resource('products', ProductController::class);
+    Route::get('productos',[ProductUserController::class, 'filter'])->name('productos');
 });
 
 Route::put('/chageStatus/{user}',[ChangeUserStatusController::class, 'update'])->name('changeUserStatus');
+Route::put('/chageProductStatus/{product}',[ChangeProductStatusController::class, 'update'])->name('changeProductStatus');
 
-Route::get('/disabled', function (){
-    return 'Su cuenta esta desabilitada';
-})->name('disabled');
+
+
+
