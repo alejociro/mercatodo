dce882aeb20494809f231899b9c59b7130dc1fcf
alejociro <alejandro.castrillonc@udea.edu.co>
2022-02-07 14:43:54 -0500

    refactor: routes

--- routes/web.php
+++ routes/web.php
@@ -8,30 +8,13 @@ use App\Http\Controllers\RolController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
 
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth','verified','check_Status'])->name('dashboard');
-
-//Route::controller(ProductUserController::class)->group(function () {
-//    Route::get('/productos', 'index');
-//});
 
 require __DIR__.'/auth.php';
 
