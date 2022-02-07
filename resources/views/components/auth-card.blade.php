030daa759169a96ed198f191594039e74740c9e2
alejociro <alejandro.castrillonc@udea.edu.co>
2022-02-07 14:49:07 -0500

    feature: add styles to auth card

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,9 +1,12 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
-        {{ $logo }}
+<section class="flex md:flex-row h-screen items-center">
+    <div class="bg-purple-600 hidden md:block w-full md:w-1/2 lg:w-2/3 h-screen">
+        <img src="https://images.pexels.com/photos/5632381/pexels-photo-5632381.jpeg?cs=srgb&dl=pexels-karolina-grabowska-5632381.jpg&fm=jpg" alt="" class="w-full h-full object-cover">
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-        {{ $slot }}
+    <div class="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 lg:w-1/3 h-screen px-6 lg:px-16 xl:px-12
+        flex items-center justify-center">
+        <div class="w-full h-100">
+            {{ $slot }}
+        </div>
     </div>
-</div>
+</section>
