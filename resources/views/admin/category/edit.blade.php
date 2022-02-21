0c2bb77c5576c3fc8c7ca7270dd63ba00aceda6a
alejociro <alejandro.castrillonc@udea.edu.co>
2022-02-21 00:28:11 -0500

    feature: views of category

---
+++ resources/views/admin/category/edit.blade.php
@@ -0,0 +1,52 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ trans('Edit a category') }}
+        </h2>
+    </x-slot>
+
+    <div class="w-full">
+        <div class="bg-gradient-to-b from-teal-800 to-teal-400 h-96"></div>
+        <div class="max-w-5xl mx-auto px-6 sm:px-6 lg:px-8 mb-12">
+            <div class="bg-white w-full shadow rounded p-8 sm:p-12 -mt-72">
+                <p class="text-3xl font-bold leading-7 text-center">Editar una categoria</p>
+                <form action="{{route('categories.update', $category)}}" method="POST">
+                    @csrf
+                    @method('PATCH')
+                    <div class="md:flex items-center mt-12">
+                        <div class="w-full md:w-1/2 flex flex-col">
+                            <label class="font-semibold leading-none">{{trans('Name')}}</label>
+                            <input id="name" name="name"
+                                   class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"
+                                   @class(['form-control', 'is-invalid' => $errors->has('name')]) value="{{$category->name}}"
+                                   type="text" required/>
+                            @error('name')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="md:flex items-center mt-8">
+                        <div class="w-full flex flex-col">
+                            <label
+                                class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">{{trans('admin.products.fields.description')}}</label>
+                            <input id="description" name="description"
+                                   class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"
+                                   @class(['form-control', 'is-invalid' => $errors->has('description')]) value="{{$category->description}}"
+                                   type="text" required/>
+                            @error('description')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class='flex items-center justify-center  md:gap-8 gap-4 pt-5 pb-5'>
+                        <a href="{{ route('categories.index') }}"
+                           class='w-auto bg-gray-500 hover:bg-gray-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>{{trans('Cancel')}}</a>
+                        <button type="submit"
+                                class='w-auto bg-gradient-to-b from-teal-800 to-teal-400 rounded-lg shadow-xl font-medium text-white px-4 py-2'>{{trans('Save')}}</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,64 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ trans('Products') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="p-2">
+                        <div class="p-6 inline-flex">
+                            <div class="inline-flex  bg-white p-2 ">
+                                <a class="inline-flex bg-indigo-200 text-black rounded-full h-6 px-3 justify-center items-center"
+                                   href="{{ route('categories.create') }}">{{trans('New')}}</a>
+                            </div>
+                        </div>
+                        <div class="bg-white rounded-lg shadow-sm text-center flex flex-col">
+                            <h1 class="bg-teal-500">{{trans('Table of categories')}}</h1>
+                            <table>
+                                <thead>
+                                <th>ID</th>
+                                <th>{{trans('Name')}}</th>
+                                <th>{{trans('Description')}}</th>
+                                </thead>
+                                <tbody>
+                                @foreach ($categories as $category)
+                                    <tr>
+                                        <td>{{$category->id}}</td>
+                                        <td>{{$category->name}}</td>
+                                        <td>{{$category->description}}</td>
+                                        <td>
+                                            <div class="inline-flex p-2">
+                                                <a href="{{ route('categories.edit', $category) }}"
+                                                   class="inline-flex items-center bg-white leading-none text-green-600 rounded-full p-2 shadow text-teal text-sm">Editar</a>
+                                                <div
+                                                    class="inline-flex items-center bg-white leading-none text-purple-600 rounded-full p-2 shadow text-teal text-sm">
+                                                    <form action="{{ route('categories.destroy', $category) }}" method="POST" class="formEliminar">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit" >Borrar</button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+
+                            </table>
+                            <div>
+                                {!! $categories->links() !!}
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
+
