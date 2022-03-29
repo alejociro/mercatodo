27e471dadbde81225b39bea86be908c0afcb99c4
alejociro <alejandro.castrillonc@udea.edu.co>
2022-03-28 21:19:03 -0500

    refactor: change model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -15,33 +16,43 @@ class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
+        'surname',
         'email',
+        'document',
+        'phone',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function shoppingCarts():HasMany
+    {
+        return $this->hasMany(ShoppingCart::class);
+    }
+
+    public function shoppingCartUser():ShoppingCart
+    {
+        return $this->shoppingCarts()->latest()->first() ?? $this->shoppingCarts()->create();
+    }
+
+    public function shoppingCartUserCreate(): ShoppingCart
+    {
+        return $this->shoppingCarts()->create();
+    }
+
+    public function payments():HasMany
+    {
+        return $this->hasMany(Payment::class);
+    }
+
+
 }
