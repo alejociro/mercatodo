515993551fa192213a352c80cde94e80173a4d8a
alejociro <alejandro.castrillonc@udea.edu.co>
2022-03-28 21:29:03 -0500

    feature: add notification of payment

---
+++ app/Notifications/PaymentNotification.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Payment;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class PaymentNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    protected Payment $payment;
+
+    public function __construct(Payment $payment)
+    {
+        $this->payment = $payment;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('An payment was created with reference' . $this->payment->reference . '.')
+                    ->action('View details', route('payments.show',$this->payment))
+                    ->line('Thanks for your purchase!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
