443cd60dbb34eb69021ae6b92b80fc4918e1bd73
alejociro <alejandro.castrillonc@udea.edu.co>
2022-03-28 21:17:59 -0500

    feature: payment gateway contract

---
+++ app/Contracts/PaymentGatewayContract.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Contracts;
+
+use App\Models\Payment;
+use App\Models\ShoppingCart;
+use Dnetix\Redirection\PlacetoPay;
+use Illuminate\Http\Request;
+
+interface PaymentGatewayContract
+{
+    public function connection(array $settings): self;
+    public function createSession(ShoppingCart $shoppingCart, Request $request): Payment;
+    public function queryPayment(Payment $payment):Payment;
+}
