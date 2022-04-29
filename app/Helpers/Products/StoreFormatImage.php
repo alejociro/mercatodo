3d08d93ca3fdf863199644eea710b5ebb18af13d
alejociro <alejandro.castrillonc@udea.edu.co>
2022-04-29 15:07:44 -0500

    feat: store image helper and action

---
+++ app/Helpers/Products/StoreFormatImage.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Helpers\Products;
+
+class StoreFormatImage
+{
+    public function saveImage($image): array
+    {
+            $routeSaveImg = 'image/';
+            $imageProduct = date('YmdHis'). "." . $image->getClientOriginalExtension();
+            $image->move($routeSaveImg, $imageProduct);
+            $data['image'] = "$imageProduct";
+
+            return $data;
+    }
+}
