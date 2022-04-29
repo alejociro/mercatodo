9ece1c2174178326151e31888f8d2a60ec95d33b
alejociro <alejandro.castrillonc@udea.edu.co>
2022-04-29 15:07:08 -0500

    feat: actions for the shopping cart

---
+++ app/Actions/User/ShoppingCart/IndexShoppingCartAction.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Actions\User\ShoppingCart;
+
+use App\Models\ShoppingCart;
+
+class IndexShoppingCartAction
+{
+    public function execute(): ShoppingCart
+    {
+        $shoppingCart = auth()->user()->shoppingCartUser();
+        $totalPrice = 0;
+        foreach ($shoppingCart->shoppingCartItems as $product){
+            $priceProduct = $product->product->price;
+            $productQuantity = $product->quantity;
+            $totalPrice += $priceProduct * $productQuantity;
+            $product->total = $product->quantity * $product->product->price;
+            $product->save();
+        }
+        $shoppingCart->total = $totalPrice;
+        $shoppingCart->save();
+
+        return $shoppingCart;
+    }
+}
---
+++ app/Actions/User/ShoppingCart/StoreItemInCartAction.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Actions\User\ShoppingCart;
+
+use App\Models\Product;
+use App\Models\ShoppingCart;
+use Illuminate\Http\RedirectResponse;
+
+class StoreItemInCartAction
+{
+    public function execute(array $data,ShoppingCart $shoppingCart, Product $product): RedirectResponse
+    {
+        $shoppingCartItem= auth()->user()->shoppingCartUser()->shoppingCartItems;
+        $itemSelected = 0;
+        $totalQuantity = 0 ;
+        foreach($shoppingCartItem as $item){
+            if($item->product_id == $product->id ){
+                $itemSelected = $item;
+                $totalQuantity = ($item->quantity) + ($data['quantity']);
+            }
+        }
+        if($totalQuantity <= $product->stock and $totalQuantity != 0){
+            $data = (['amount' => $totalQuantity]);
+            $itemSelected->update($data);
+            return redirect()->route('shoppingCartUser');
+        }elseif ($totalQuantity > $product->stock){
+            return redirect()->route('shoppingCartUser');
+        }
+        if($data['quantity'] <= $product->stock){
+            $shoppingCart->shoppingCartItems()->create([
+                'product_id' => $product->getKey(),
+                'quantity' => $data['quantity'],
+                'total' => $product->price
+            ]);
+            return redirect()->route('productos');
+        } else {
+            return redirect()->route('productosr');
+        }
+    }
+}
