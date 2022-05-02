82e64d4bb63257c3e98ac240137addd8a2450d15
alejociro <alejandro.castrillonc@udea.edu.co>
2022-05-01 19:37:20 -0500

    feat: actions export controller

---
+++ app/Http/Controllers/Admin/User/ExportActionsController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Admin\User;
+
+use App\Exports\ActionsExport;
+use App\Http\Controllers\Controller;
+use App\Jobs\NotifyUserOfCompletedExport;
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+
+class ExportActionsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('permission:create-user|edit-user|delete-user', ['only'=>['form','export']]);
+    }
+
+    public function form()
+    {
+        return view('admin.users.actions.export');
+    }
+
+    public function export(Request $request)
+    {
+        $user =auth()->user();
+        $filepath = asset('storage/actions.xlsx');
+        Excel::store((new ActionsExport)
+            ->forUser($request->query('user_id')), 'actions.xlsx','public')
+            ->chain([new NotifyUserOfCompletedExport($user,$filepath)]);
+
+        return view('admin.users.actions.export');
+    }
+}
