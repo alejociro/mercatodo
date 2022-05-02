afe180a7b8d987485452f2a61a26ecc29a5f9a23
alejociro <alejandro.castrillonc@udea.edu.co>
2022-05-01 20:26:44 -0500

    refactor: product migration

--- database/migrations/2022_01_11_032210_create_products_table.php
+++ database/migrations/2022_01_11_032210_create_products_table.php
@@ -11,7 +11,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name', 120)->unique();
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->unsignedBigInteger('price');
             $table->unsignedBigInteger('stock');
             $table->tinyText('description');
