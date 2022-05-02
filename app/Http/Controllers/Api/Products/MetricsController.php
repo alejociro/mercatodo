c311a1bd4103fe9c43a256df8c063714842d9cc6
alejociro <alejandro.castrillonc@udea.edu.co>
2022-05-01 19:52:04 -0500

    feat: controller of the metric of products sold

---
+++ app/Http/Controllers/Api/Products/MetricsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api\Products;
+
+use App\Http\Controllers\Controller;
+use App\Models\Payment;
+use App\Models\ProductSold;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
+
+class MetricsController extends Controller
+{
+    public function chart()
+    {
+        $values = ProductSold::select('name', DB::raw('count(*) as total'))->groupBy('name')
+            ->get()
+            ->toArray();
+
+        return response()->json([
+            'data' => Arr::pluck($values, 'total', 'name')
+        ]);
+    }
+
+    public function barChart()
+    {
+        $values = Payment::select('status', DB::raw('count(*) as total'))->groupBy('status')
+            ->get()
+            ->toArray();
+
+        return response()->json([
+            'data' => Arr::pluck($values, 'total', 'status')
+        ]);
+    }
+}
